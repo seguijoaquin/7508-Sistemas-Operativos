b5292cb60e17bf1d41154bce47e9c55e7e98c4ca
Sebas <mennitise@gmail.com>
2016-04-30 18:46:44 -0300

    ProcesarOfertas.sh: Agrego mas validaciones

--- ProcesarOfertas.sh
+++ ProcesarOfertas.sh
@@ -27,8 +27,18 @@ function existeDuplicado()
 function formatoCamposValido()
 {
 	nroCamposPrimerRegistro=`head -1 "$OKDIR/$1" | sed s/";"/" "/ | wc -w`
-
 	if [ $nroCamposPrimerRegistro -eq 2 ]
+	then # Posee formato valido
+		return 0
+	fi
+	# Posee formato invalido
+	return 1
+}
+
+function formatoContratoValido()
+{
+	contratoConFormatoValido=`echo $1 | grep "^[0-9]\{7\};"`
+	if [[ ! -z $contratoConFormatoValido ]]
 	then # Posee formato valido
 		return 0
 	fi
@@ -38,12 +48,49 @@ function formatoCamposValido()
 
 function ofertaValida()
 {
-	#TODO: Implementar
-	return 0
+	msg=""
+	if formatoContratoValido $1
+	then # Verifica que el formato del contrato fusionado sea correcto
+
+		suscriptor=`grep --text "^${1:0:4};${1:4:3}" $SUSCRIPTORES`
+		if [[ ! -z $suscriptor ]]
+		then # Valida el suscriptor contra el padrón de suscriptores.
+
+			grupoAbierto=`grep ${suscriptor:0:4} $GRUPOS | grep "ABIERTO"`
+			grupoNuevo=`grep ${suscriptor:0:4} $GRUPOS | grep "NUEVO"`
+
+			if [[ ! -z $grupoAbierto ]] || [[ ! -z $grupoNuevo ]]
+			then # Valida que el grupo este ABIERTO o NUEVO
+
+				participa=`echo $suscriptor | cut -f 6 -d";"`
+
+				if [[ $participa = "1" ]] || [[ $participa = "2" ]]
+				then # Valida que el suscriptor participe
+
+					#TODO: Validar importe. Mayor o igual al monto mínimo (valor de cuota pura * cantidad de cuotas para licitación)
+					#TODO: Validar importe. Menor o igual al monto máximo (valor de cuota pura * cantidad de cuotas pendientes)
+
+					return 0
+
+				else msg="EL SUSCRIPTOR NO PARTICIPA";
+				fi
+
+			else msg="GRUPO CERRADO";
+			fi
+
+		else msg="SUSCRIPTOR NO EXISTENTE";
+		fi
+
+	else msg="FORMATO DE REGISTROS NO VALIDO";
+	fi
+
+	return 1
 }
 
 #--------------------------------------------------------------------------------------------------------#
 
+cantidadOfertasValidas=0
+cantidadOfertasRechazadas=0
 cantidadArchivosOfertas=`ls -1 $OKDIR | wc -w` # Calcula cantidad de archivos a procesar
 
 ./GrabarBitacora.sh "ProcesarOfertas" "Inicio de ProcesarOfertas" "INFO"
@@ -53,8 +100,6 @@ listaArchivos=`ls -1 $OKDIR | sort -t _ -k2` # Ordeno los archivos cronologicame
 for archivo in $listaArchivos
 do
 	#Proceso los archivos
-	
-	#echo "Proceso archivo $archivo"
 
 	# 2.1 Verificar que no sea un archivo duplicado
 	if [ ! `existeDuplicado $archivo` ]
@@ -66,18 +111,22 @@ do
 			./GrabarBitacora.sh "ProcesarOfertas" "Archivo a procesar: $archivo" "INFO"
 
 			# 4 Validar oferta
-			for oferta in `more "$OKDIR/lear$archivo"`
+			for oferta in `more "$OKDIR/$archivo"`
 			do
 				if ofertaValida $oferta
 				then
 					# 5 Grabar oferta valida en el registro de ofertas validas
-					echo "grabo oferta"
+					#TODO: Grabar oferta valida en el registro de ofertas validas
+					let cantidadOfertasValidas++
 				else
 					# 6 Rechazar oferta, en el caso que alguna de las validaciones sea invalida,
 					# grabando en el registro de ofertas rechazadas
-					echo "rechazo oferta"
+					#TODO: Grabar oferta rechazada.
+					let cantidadOfertasRechazadas++
 				fi
 			done
+			let total=cantidadOfertasValidas+cantidadOfertasRechazadas
+			./GrabarBitacora.sh "ProcesarOfertas" "Registros leidos = $total; Ofertas validas = $cantidadOfertasValidas - Ofertas rechazadas = $cantidadOfertasRechazadas " "INFO"
 		else
 			#TODO: Rechazar archivo por formato de primer registro invalido, con el mensaje “Se  rechaza  el  archivo  porque  su  estructura  no  se  corresponde  con  el formato esperado“
 			echo “Se  rechaza  el  archivo  porque  su  estructura  no  se  corresponde  con  el formato esperado“
@@ -86,5 +135,7 @@ do
 		#TODO: Rechazar archivo con el mensaje “Se rechaza el archivo por estar DUPLICADO“
 		echo “Se rechaza el archivo por estar DUPLICADO“
 	fi
+	cantidadOfertasValidas=0
+	cantidadOfertasRechazadas=0
 done
 
