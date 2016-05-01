8275e663fcde4e0677aa46b4302f6031b330ac9e
edgardolopez <edgardo.skl@gmail.com>
2016-05-01 20:46:40 -0300

    agrego variable de backup en conf

--- INSTALL.sh
+++ INSTALL.sh
@@ -441,13 +441,14 @@ then
 	LOGEXTTMP=$(grep '^LOGEXT' $CONFIGFILETEMP | awk -F"=" '{print $2}' )
 	LOGSIZETMP=$(grep '^LOGSIZE' $CONFIGFILETEMP | awk -F"=" '{print $2}' )
 	NOKDIRTMP=$(grep '^NOKDIR' $CONFIGFILETEMP | awk -F"=" '{print $2}' )
+	BACKUPDIRTMP=$(grep '^BACKUPDIR' $CONFIGFILETEMP | awk -F"=" '{print $2}' )
 
 	declare -a VAR_FALTANTES; #Array con los directorios que falta configurar
 	declare -a VAR_COMPLETO; #Array con los directorios que falta configurar
 
 	if [ -z $BASEDIRTMP ]
 	then
-		echo "GRUPO=$BASEDIR=$USER=`date +'%d-%m-%Y %H:%M:%S'`" >> $CONFIGFILETEMP
+		echo "GRUPO=$GRUPO=$USER=`date +'%d-%m-%Y %H:%M:%S'`" >> $CONFIGFILETEMP
 	fi
 
 	if [ -z $BINDIRTMP ]
@@ -537,6 +538,8 @@ then
 		NOKDIR=$NOKDIRTMP
 		VAR_COMPLETO=( ${VAR_COMPLETO[*]} NOKDIR)
 	fi
+
+	
 
 	if [ ${#VAR_FALTANTES[@]} -eq 0 ] #No falta ninguna variable en el archivo temporal
 	then
@@ -692,6 +695,10 @@ then
 	done
 	fi
 
+	if [ -z $BACKUPDIRTMP ]
+	then
+		echo "BACKUPDIR=$BACKUPDIR=$USER=`date +'%d-%m-%Y %H:%M:%S'`" >> $CONFIGFILETEMP
+	fi
 	executeInstaler "LISTA"
 
 else
