bd919fc15425ef868054ebf3e0c7278909907401
gon222000 <gon.m.cuevas@gmail.com>
2016-05-01 13:12:16 -0300

    Added files via upload
    Agrego ganadores por sorteo

--- DeterminarGanadores.pl
+++ DeterminarGanadores.pl
@@ -7,6 +7,9 @@ use warnings;
 use Text::CSV;
 
 my %opts;
+my %padron;
+my %ganadores_sorteo;
+my %ganadores_licitacion;
 getopts('ag', \%opts) or die mostrar_ayuda();
 
 if(defined $opts{a})
@@ -75,6 +78,98 @@ sub procesar
 sub ejecutar_proceso
 {
 	#TODO: Logica del proceso
+	ganadores_por_sorteo();
+}
+
+sub ganadores_por_sorteo
+{
+	my $csv_padron = Text::CSV->new({ caracter => ';' });
+	my $file_padron = "archivo padron";	#TODO: poner nombre archivo correcto
+	open(my $data_padron, '<', $file_padron) or die "No se puede abrir el archivo '$file_padron' $!\n";
+	
+	my $csv_sorteo = Text::CSV->new({ caracter => ';' });
+	my $file_sorteo = "archivo sorteo";	#TODO: poner nombre archivo correcto
+	open(my $data_sorteo, '<', $file_sorteo) or die "No se puede abrir el archivo '$file_sorteo' $!\n";
+	
+	my $csv_grupo = Text::CSV->new({ caracter => ';' });
+	my $file_grupo = "archivo grupo";	#TODO: poner nombre archivo correcto
+	open(my $data_grupo, '<', $file_grupo) or die "No se puede abrir el archivo '$file_grupo' $!\n";
+	
+	my @datos_ganador; #Array con los datos del ganador
+	my $numero_menor = 168;
+	my $participa = 0;
+	
+	foreach my $g (@lista_grupos) {
+		#Analizo si el grupo participa - grupo ABIERTO
+		$participa = 0;
+		while (my $line_grupo = <$data_grupo>) {
+		  chomp $line_grupo;
+		  if ($csv_grupo->parse($line_grupo)) {
+			  my @fields_grupo = $csv_grupo->fields_grupo();
+			  if($fields_grupo[0] == $g)
+			  {
+				if($fields_grupo[1] eq "ABIERTO")
+				{
+					$participa = 1;
+				}
+			  }
+		  } else {
+			  warn "Error en el formato de línea en: $line_padron\n";
+		  }
+		}
+	
+		if($participa == 1)
+		{
+			#Cargo el nombre y el orden segun el archivo de padrones
+			while (my $line_padron = <$data_padron>) {
+			  chomp $line_padron;
+			  if ($csv_padron->parse($line_padron)) {
+				  my @fields_padron = $csv_padron->fields_padron();
+				  if($fields_padron[0] == $g) #Si el campo grupo es el grupo sobre el que estoy procesando
+				  {
+					if(($fields_padron[5] == 1) || ($fields_padron[5] == 2)) #Si participa, lo agrego al hash
+					{
+						$padron{$fields_padron[1]} = $fields_padron[2];
+					}
+				  }
+			  } else {
+				  warn "Error en el formato de línea en: $line_padron\n";
+			  }
+			}
+			
+			#Veo ganador segun el archivo de sorteos
+			$numero_menor = 168;
+			@datos_ganador=(); 
+			
+			while (my $line_sorteo = <$data_sorteo>) {
+			  chomp $line_sorteo;
+			  if ($csv_sorteo->parse($line_sorteo)) {
+				  my @fields_sorteo = $csv_sorteo->fields_sorteo();
+				  for my $p (keys %padron) {
+					 if($fields_sorteo[0] == $p) 
+					 {
+						if($fields_sorteo[1] < $numero_menor)
+						{
+							$numero_menor = $fields_sorteo[1];
+							$datos_ganador.push($p); #Orden
+							$datos_ganador.push($fields_sorteo[1]); #Sorteo
+							$datos_ganador.push($padron{$p}); #Nombre
+						}
+					 }
+				  }
+			  } else {
+				  warn "Error en el formato de línea en: $line_sorteo\n";
+			  }
+			}
+			#Meto en el hash como clave el sorteo y como dato el orden
+			$ganadores_sorteo{$g} = $datos_ganador;
+		}
+	}
+}
+
+sub ganadores_por_licitacion
+{
+
 }
 
 sub analizar_grupos
@@ -94,7 +189,12 @@ sub analizar_grupos
 
 sub mostrar_resultados_grupo
 {
-	#TODO: Mostrar resultados por grupo
+	#TODO: poner el titulo
+	foreach my $s (keys %ganadores_sorteo) {	
+			print "$s - $ganadores_sorteo{$s}[0] S $ganadores_sorteo{$s}[2]\n";
+			print "$s - $ganadores_licitacion{$s}[0] L $ganadores_licitacion{$s}[2]\n";
+		}
+	}
 }
 
 sub mostrar_ganadores_licitacion
@@ -104,14 +204,16 @@ sub mostrar_ganadores_licitacion
 
 sub mostrar_ganadores_sorteo
 {
-	#TODO: Mostrar ganadores sorteo
+	#TODO: poner el titulo
+	foreach my $g (keys %ganadores_sorteo) {
+			print "Ganador por sorteo del grupo $g Nro de Orden: $ganadores_sorteo{$g}[0], $ganadores_sorteo{$g}[2] (Nro deSorteo $ganadores_sorteo{$g}[1])\n";
+	}
 }
 
 sub mostrar_resutado_general
 {
-	my $csv = Text::CSV->new({ caracter => ',' });
-	#TODO: poner nombre archivo correcto
-	my $file = "archivo sorteo";
+	my $csv = Text::CSV->new({ caracter => ';' });
+	my $file = "archivo sorteo";	#TODO: poner nombre archivo correcto
 	open(my $data, '<', $file) or die "No se puede abrir el archivo '$file' $!\n";
 	while (my $line = <$data>) {
 	  chomp $line;
